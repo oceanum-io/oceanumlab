bb3671013b1ca9ae4045452ee2904db1abd34f4d
dave <d.johnson@oceanum.science>
2023-09-12 16:12:55 +1200

    Remove legacy startup endpoints

--- oceanumlab/__init__.py
+++ oceanumlab/__init__.py
@@ -7,13 +7,6 @@ from .handlers import setup_handlers
 
 HERE = Path(__file__).parent.resolve()
 
-with (HERE / "labextension" / "package.json").open() as fid:
-    data = json.load(fid)
-
-
-def _jupyter_labextension_paths():
-    return [{"src": "labextension", "dest": data["name"]}]
-
 
 def _jupyter_server_extension_points():
     return [{"module": "oceanumlab"}]
